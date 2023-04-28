57785f215ddc17fb29d4f6699130fddc8f1bd8df
Ferge Mate <fergemate@gmail.com>
2023-04-28 13:09:04 +0200

    Merge Conflict B

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -75,7 +75,7 @@ public class GT4500 implements SpaceShip {
           }
         }
         break;
-
+//asdhjkjasdasdasasdasdasdasd
       case ALL:
         // try to fire both of the torpedo stores
         //TODO implement feature
